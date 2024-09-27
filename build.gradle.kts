4794097c96558375580510a736f3fb24f3bf256d
deepak-jain-agrovet <123359855+deepak-jain-agrovet@users.noreply.github.com>
2024-09-27 17:22:16 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android)  apply false
-    alias(libs.plugins.navigation.safeargs) apply false
+    alias(libs.plugins.navigation.safeargs) version "2.8.1" apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
 }
